fefe6d38ab55f9ea974030132348c3eecb0d2012
David Guo <dvguo@ucdavis.edu>
2020-05-14 01:39:04 -0700

    Implemented contact sidebar
    -Basic position is complete
    -Font, fontSize, color will need to be changed

---
+++ chat-signal/src/components/contactsSidebar.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+
+class ContactCell extends Component {
+  render() {
+    return (
+      <div style={this.props.style}>
+        <div style={Styles.blubStyle}>{this.props.initial}</div>
+        <div style={Styles.infoStyle}>
+          <div style={Styles.contactNameStyle}>{this.props.name}</div>
+          <div style={Styles.contactStatusStyle}>{this.props.status}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ContactsSidebar extends Component {
+  state = {
+    contacts: [
+      { initial: "A", name: "Andy Wu", status: "User is typing..." },
+      { initial: "C", name: "Corbin Harell", status: "1 New Message" },
+      { initial: "J", name: "Jason Lin", status: "User is typing..." },
+    ],
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <ContactCell
+          style={Styles.contactStyle}
+          initial="D"
+          name="David Guo"
+          status="online"
+        />
+
+        <div style={Styles.btnStyle}>
+          <div>CONTACTS</div>
+          <div> | </div>
+          <div>NEW CHAT</div>
+        </div>
+
+        {this.state.contacts.map((cell) => (
+          <ContactCell
+            style={Styles.contactStyle}
+            initial={cell.initial}
+            name={cell.name}
+            status={cell.status}
+          />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+const Styles = {
+  contactStyle: {
+    display: "flex",
+    backgroundColor: "#2F455C",
+    fontSize: 25,
+    color: "#84878A",
+    margin: 3,
+  },
+  btnStyle: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  blubStyle: {
+    display: "flex",
+    flexGrow: 1,
+
+    justifyContent: "center",
+    alignContent: "center",
+
+    width: 50,
+    height: 50,
+    maxWidth: 50,
+
+    fontSize: 25,
+    backgroundColor: "white",
+    color: "#B2B2B2",
+    borderRadius: 1000,
+    margin: 10,
+  },
+  infoStyle: {
+    display: "flex",
+    flexGrow: 12,
+    flexDirection: "column",
+    margin: 10,
+    color: "#B2B2B2",
+    backgroundColor: "#257869",
+  },
+  contactNameStyle: {
+    display: "flex",
+    fontSize: 20,
+    margin: 1,
+    color: "#B2B2B2",
+  },
+  contactStatusStyle: {
+    display: "flex",
+    fontSize: 17,
+    margin: 1,
+    color: "green",
+  },
+};
+export default ContactsSidebar;
--- chat-signal/src/index.js
+++ chat-signal/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ContactsSidebar from "./components/contactsSidebar";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <ContactsSidebar />,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
