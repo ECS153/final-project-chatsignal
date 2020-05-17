c1c97fcfb5847e359e2e0649a1ace148c5f8f86d
andywu0408 <andy@japa.one>
2020-05-16 20:50:00 -0700

    Resolve conflict

--- chat-signal/src/ChatRoom/MsgInputBox.js
+++ chat-signal/src/ChatRoom/MsgInputBox.js
@@ -4,30 +4,17 @@ import { Input } from "antd";
 const { TextArea } = Input;
 
 const MsgInputBox = (props) => {
-<<<<<<< HEAD
 
-    return (
-        <div style={Styles.mainContainer}>
-            <TextArea
-                placeholder="Type a message..."
-                autoSize={{ minRows: 6, maxRows: 6 }}
-
-                style={Styles.textAreaStyle}
-            />
-        </div>
-    );
-=======
   return (
     <div style={Styles.mainContainer}>
       <TextArea
         placeholder="Type a message..."
         autoSize={{ minRows: 6, maxRows: 6 }}
-        maxLength={1000}
+
         style={Styles.textAreaStyle}
       />
     </div>
   );
->>>>>>> 3b37700f4827477791a50f0028c90f37305319e8
 };
 
 const Styles = {
