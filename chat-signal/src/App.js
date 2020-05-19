67d36f4e44840b4f080a4ddf873d478b9b38ce91
Andy Wu <andywu0408@gmail.com>
2020-05-18 21:01:34 -0700

    Integrate login components

--- chat-signal/src/App.js
+++ chat-signal/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import LoginScreen from "./login.js";
+import LoginPage from "./Login/LoginPage";
 import ChatRoom from "./ChatRoom/index.jsx";
+
 function App() {
   return (
     // Add css styling like below
     <div>
       {/* <LoginScreen /> */}
-      <ChatRoom />
+      {/* <ChatRoom /> */}
+      <LoginPage />
     </div>
   );
 }
--- chat-signal/src/login/login.jsx
+++ chat-signal/src/login/login.jsx
@@ -11,16 +11,16 @@ export class Login extends React.Component {
             {/* <div className="header">Login</div> */}
             <div className="content">
                 <div className="image">
-                    <img src={logoImg} alt="Logo"/>
+                    <img src={logoImg} alt="Logo" />
                 </div>
                 <div className="form">
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input type="text" name="username" placeholder="username"/>
+                        <input type="text" name="username" placeholder="username" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="text" name="password" placeholder="password"/>
+                        <input type="text" name="password" placeholder="password" />
                     </div>
                 </div>
             </div>
