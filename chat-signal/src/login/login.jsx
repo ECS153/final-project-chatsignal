7ee8df021d7b0a0ed5e5c935dee2d9d29fa753c4
Andy Wu <andywu0408@gmail.com>
2020-05-18 23:31:50 -0700

    Go to chatroom screen on successful login

--- chat-signal/src/login/login.jsx
+++ chat-signal/src/login/login.jsx
@@ -1,30 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import logoImg from "../chatsignal.png"
+import { message } from 'antd';
 
-
-//TODO: Need to add login authentication. Only change "isLoginVerified" to true after successful verification
+//TODO: implement authentication.
 export const Login = () => {
     let history = useHistory();
-    /*
-    NOTE for the team:
-        Will navigate to chatroom screen once isLoginVerified is set to true;
-        p.s. to set it, do it like this -> setIsLoginVerified(true)
-        p.s. this is react hooks with JS es6. (new/clean ways to write react component)
-    */
-    const [isLoginVerified, setIsLoginVerified] = useState(false);
+    // NOTE: Will navigate to chatroom screen once isLoginVerified is set to true;
+    let isLoginVerified = false;
+
+
+    //TODO: implement authentication in authLoginInfo() below
+    const authLoginInfo = () => {
+
+        //if auth succeed
+        isLoginVerified = true;
+        //else{
+        //     isLoginVerified = false;
+        // }
+    }
 
     const onLoginPressed = () => {
-        console.log("User pressed on login button...");
-        //TODO: Add a function to verify login & update isLoginVerified accordingly
-        // if (isLoginVerified) {
-        history.push('/chatroom');
-        // }
 
+        authLoginInfo();
+
+        if (isLoginVerified) {
+            history.push('/chatroom');
+            message.success('Logged in successfully. Start chatting!')
+        } else {
+            message.error('Login failed. Please try again.');
+        }
     }
     return (
         <div className="base-container">
-            {/* <div className="header">Login</div> */}
             <div className="content">
                 <div className="image">
                     <img src={logoImg} alt="Logo" />
