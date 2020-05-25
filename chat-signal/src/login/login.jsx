c77ec7093bc578ccd97db4a747dce5506326dfff
Wei Xuan (Nathan) Ng <wxng@ucdavis.edu>
2020-05-26 00:42:43 +0800

    Get Req from DB

--- chat-signal/src/login/login.jsx
+++ chat-signal/src/login/login.jsx
@@ -1,23 +1,10 @@
 import React, { useState, useEffect, useCallback, Component } from "react";
 import { useHistory } from "react-router-dom";
 import logoImg from "../chatsignal.png"
-import { message } from 'antd';
+import { message, InputNumber } from 'antd';
 import useForceUpdate from 'use-force-update';
 import axios from 'axios';
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
-
-// var AWS = require("aws-sdk");
-// aws_access_key_id = AKIAUGOGGDYPDWITXGMT;
-// aws_secret_access_key = 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;
-// var myConfig = AWS.config.update({
-//     aws_access_key_id : "AKIAUGOGGDYPCSPZ5RB6",
-//     region: "us-west-2",
-//     endpoint: "https://e770o4wls8.execute-api.us-west-2.amazonaws.com/prod"
-//   });
-// var docClient = new AWS.DynamoDB.DocumentClient();
-// var hash;
-// AWS.config = myConfig;
-
 
 //TODO: implement authentication.
 export const Login = () => {
@@ -25,10 +12,12 @@ export const Login = () => {
     const [password, getPassword] = useState(0); // react hooks
     const [ipAddress, getIP] = useState(0); // react hooks
     const [city, getCity] = useState(0); // react hooks
+    const [tasks, getTasks] = useState(0); // react hooks
     const forceUpdate = useForceUpdate();
     let history = useHistory();
     // NOTE: Will navigate to chatroom screen once isLoginVerified is set to true;
     let isLoginVerified = false;
+    let retrieveUser = "";
 
     //TODO: implement authentication in authLoginInfo() below
     const authLoginInfo = () => {
@@ -42,17 +31,34 @@ export const Login = () => {
 
     function handleUsername(event) {
         getUsername(event.target.value);
-        
     }
     
     function handlePassword(event) {
         getPassword(event.target.value);
-       
+    }
+    
+    function handleTasks(event) {
+        getTasks(event.target.value);
+    }
+
+    function fetchUserInfo(){
+        var request = fetch('https://e770o4wls8.execute-api.us-west-2.amazonaws.com/prod',{
+            method: 'GET',
+            headers:{},
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.body);
+            // handleTasks(response.tasks);
+        });
     }
 
     useEffect(() => {
-        var JSONpackage = { username: username, password: password, ip: ipAddress, location: city }
+        var JSONpackage = { username: username, password: password, location: city }
         console.log(JSONpackage); // here's the information in JSON format
+        if (city != "") {
+            authenticate();
+        }
     }, [city]);
 
     function handleCity(tempCity) {
@@ -64,40 +70,28 @@ export const Login = () => {
             .then(res => {
                 return res.text()
             }).then(ip => {
-                getIP(ip);
+                var endpoint = "http://ip-api.com/json/" + ip + "?fields=city";
+                fetch(endpoint)
+                    .then(response => response.json())
+                    .then(response => {
+                        handleCity(response.city);
+                    });
             });
-        var endpoint = "http://ip-api.com/json/" + ipAddress + "?fields=city";
-        fetch(endpoint)
-            .then(response => response.json())
-            .then(response => {
-                handleCity(response.city);
-            });
+        
     }
     
     
 
-    // function authenticate() {
-    //     var trueFalse = verifyHash();
-    //     console.log("HERE");
-    //     if (trueFalse){
-    //         isLoginVerified = true;
-    //     }
-    //     console.log(isLoginVerified);
-    //     var params = { TableName: "AccountDB",
-    //         Key:{
-    //             "UserID" : "Jason", 
-    //             "Email" : "jason@gmail.com", 
-    //             "Location" : "Singapore", 
-    //             "Password" : "$2a$10$fs7pMJBwBUHx/twmteN20u/20E4/Fkfv/0Qy3RUbuzkXD5.dXzssm"
-    //         } }
-    //     docClient.query(params, function(err, data) {
-    //         if (err) {
-    //             console.error("Invalid Password. ", JSON.stringify(err, null, 2));
-    //         } else {
-    //             console.log("Valid Password, login succeeded:", JSON.stringify(data, null, 2));
-    //         }
-    //     });
-    // }
+    function authenticate() {
+        fetchUserInfo();
+        var trueFalse = verifyHash();
+        console.log("HERE");
+        if (trueFalse){
+            isLoginVerified = true;
+        }
+        console.log(isLoginVerified);
+        console.log("I made it here");
+    }
     
     function verifyHash(event) {
         var hash = "$2a$10$fs7pMJBwBUHx/twmteN20u/20E4/Fkfv/0Qy3RUbuzkXD5.dXzssm";
@@ -123,8 +117,7 @@ export const Login = () => {
         } else {
             message.error('Login failed. Please try again.');
         }
-        // handleCity();    
-        // authenticate();
+        handleCity();    
     }
     return (
         <div className="base-container">
